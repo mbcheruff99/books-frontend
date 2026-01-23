1f6414b0e8df0f5b1fb19b5e53e68aea438dd917
bshin27 <125755440+bshin27@users.noreply.github.com>
2026-01-22 22:48:46 -0500

    added the .json requirement to api requests

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -6,6 +6,7 @@ export default function BookCard({ book, userShelves, onAdded }) {
   const [loading, setLoading] = useState(false);
 
   async function addToShelf() {
+
     if (!selectedShelf) return alert("Please select a shelf first!");
     setLoading(true);
     try {
@@ -51,37 +52,3 @@ export default function BookCard({ book, userShelves, onAdded }) {
     </div>
   );
 }
-
-
-// // import { useState } from "react";
-// import api from "../api/api"
-// import { useNavigate } from "react-router-dom";
-
-// export default function BookCard({ book, shelfId }) {
-//   const navigate = useNavigate()
-  
-//   async function addToShelf(bookId) {
-//    try {
-//       await api.post("/shelvings", { shelf_id: shelfId, book_id: bookId });
-//       if (window.confirm("Book added! See it in your shelf")) { // ?????
-//         navigate("/shelves");
-//       }
-//     } catch (err) {
-//       console.error(err);
-//       alert("Failed to add book to shelf");
-//     }
-//   }
-
-
-//   return (
-//     <div className="card h-100">
-//       <img src={book.cover} className="card-img-top" alt={book.title} />
-//       <div className="card-body">
-//         <h5 className="card-title">{book.title}</h5>
-//         <p className="card-text">{book.author}</p>
-//         <button className="btn btn-sm btn-success" onClick={() => addToShelf(book.id)}>Add to Read</button>
-//       </div>
-//     </div>
-//   );
-  
-// }
