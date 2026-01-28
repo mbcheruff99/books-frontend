a5fba71f3dd002037a9eb2132d2823f7caee9eba
bshin27 <125755440+bshin27@users.noreply.github.com>
2026-01-27 23:04:59 -0500

    fixed api.js to get deployment working

--- src/api/api.js
+++ src/api/api.js
@@ -1,18 +1,17 @@
 import axios from "axios";
 
-// Allows get requests to be written in /... format without localhost and .json
+const baseURL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000" // for local dev
+    : "https://bookshelf-q8nj.onrender.com"; // your deployed backend
 
 const api = axios.create({
-  baseURL: "http://localhost:3000", 
+  baseURL,
   withCredentials: true,  
   headers: {
     Accept: "application/json",
-    "Content-Type": "application/json"
+    "Content-Type": "application/json",
   },         
 });
 
 export default api;
-
-axios.defaults.baseURL = process.env.NODE_ENV === "development" 
-  ? "http://localhost:3000" 
-  : "https://mini-capstone-api-abc123.onrender.chttps://bookshelf-q8nj.onrender.comom";  
